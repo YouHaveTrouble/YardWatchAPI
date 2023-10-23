fd6a2bfce5628838f44ff0ea60e36aed861df968
youhavetrouble <youhavetrouble@youhavetrouble.me>
2023-10-24 00:11:05 +0200

    protecc

---
+++ readme.md
@@ -0,0 +1,4 @@
+# YardWatch
+
+API to unify protection plugins for minecraft bukkit servers to allow easy protection queries without having to import
+10 different plugin apis with separate implementations.
---
+++ src/main/java/me/youhavetrouble/yardwatch/Protection.java
@@ -0,0 +1,67 @@
+package me.youhavetrouble.yardwatch;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+/**
+ * Interface for protection plugins
+ */
+public interface Protection {
+
+    /**
+     * Check if the implementing plugin is enabled
+     * @return true if enabled, false if not
+     */
+    boolean isEnabled();
+
+    /**
+     * Check if location is protected. This is for plugins that place or break blocks without a player involved.
+     * @param location location to check
+     * @return true if location is protected, false if not
+     */
+    boolean isProtected(Location location);
+
+    /**
+     * Check if player can break specific block
+     * @param player player
+     * @param blockState block to check. This should always be a snapshot and not live object.
+     * @return true if player can break block, false if not
+     */
+    boolean canBreakBlock(Player player, BlockState blockState);
+
+    /**
+     * Check if player can place block in specific location
+     * @param player player
+     * @param location location to check
+     * @return true if player can place block, false if not
+     */
+    boolean canPlaceBlock(Player player, Location location);
+
+    /**
+     * Check if player can interact with specific block
+     * @param player player
+     * @param block block to check
+     * @return true if player can interact with block, false if not
+     */
+    boolean canInteract(Player player, Block block);
+
+    /**
+     * Check if player can interact with specific entity
+     * @param player player
+     * @param target target entity
+     * @return true if player can interact with entity, false if not
+     */
+    boolean canInteract(Player player, Entity target);
+
+    /**
+     * Check if player can damage specific entity
+     * @param damager entity that is to damage target
+     * @param target target entity
+     * @return true if player can damage entity, false if not
+     */
+    boolean canDamage(Entity damager, Entity target);
+
+}
